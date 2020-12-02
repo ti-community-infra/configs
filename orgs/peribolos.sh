146f94e5033f52b3bbe4190555717ed72fc80f76
hi-rustin <rustin.liu@gmail.com>
2020-12-02 20:19:32 +0800

    config: add echo

--- orgs/peribolos.sh
+++ orgs/peribolos.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+echo "Build merge and peribolos tools..."
 go build -o tools/bin/merge cmd/merge/main.go
 go build -o tools/bin/peribolos k8s.io/test-infra/prow/cmd/peribolos
 
+echo "Generate configuration file:"
 tools/bin/merge --org-part=tidb-community-bots=orgs/tidb-community-bots/org.yaml --merge-teams=true 2>&1 | tee config.yaml
+
+echo " $(color-green 'done'), Start peribolos..."
+for s in {5..1}; do
+    echo -n $'\r'"in $s..."
+    sleep 1s
+done
 
 tools/bin/peribolos "$@"
