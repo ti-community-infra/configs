1bcff77ade821e2de535694c2766e99d1587a9c7
hi-rustin <rustin.liu@gmail.com>
2020-12-02 20:10:24 +0800

    config: add env

--- orgs/peribolos.sh
+++ orgs/peribolos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-GO111MODULE=on go build -o tools/bin/merge cmd/merge/main.go
-GO111MODULE=on go build -o tools/bin/peribolos k8s.io/test-infra/prow/cmd/peribolos
+go build -o tools/bin/merge cmd/merge/main.go
+go build -o tools/bin/peribolos k8s.io/test-infra/prow/cmd/peribolos
 
 tools/bin/merge --org-part=tidb-community-bots=orgs/tidb-community-bots/org.yaml --merge-teams=true 2>&1 | tee config.yaml
 
