a892a52519af1135e67f7d0f3db4ba58cb67cf24
hi-rustin <rustin.liu@gmail.com>
2020-12-02 20:24:18 +0800

    config: fix job

--- orgs/peribolos.sh
+++ orgs/peribolos.sh
@@ -7,10 +7,5 @@ go build -o tools/bin/peribolos k8s.io/test-infra/prow/cmd/peribolos
 echo "Generate configuration file:"
 tools/bin/merge --org-part=tidb-community-bots=orgs/tidb-community-bots/org.yaml --merge-teams=true 2>&1 | tee config.yaml
 
-echo " $(color-green 'done'), Start peribolos..."
-for s in {5..1}; do
-    echo -n $'\r'"in $s..."
-    sleep 1s
-done
-
+echo "Start peribolos..."
 tools/bin/peribolos "$@"
