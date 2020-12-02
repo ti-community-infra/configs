8d4ee11c034bfce62f45edf4e669c7be9d4a5cd3
hi-rustin <rustin.liu@gmail.com>
2020-12-02 20:03:45 +0800

    config: fix typo

--- orgs/peribolos.sh
+++ orgs/peribolos.sh
@@ -3,6 +3,6 @@
 GO111MODULE=on go build -o tools/bin/merge cmd/merge/main.go
 GO111MODULE=on go build -o tools/bin/peribolos k8s.io/test-infra/prow/cmd/peribolos
 
-tools/bin/merge --org-part=tidb-community-bots=./tidb-community-bots/org.yaml --merge-teams=true 2>&1 | tee config.yaml
+tools/bin/merge --org-part=tidb-community-bots=orgs/tidb-community-bots/org.yaml --merge-teams=true 2>&1 | tee config.yaml
 
 tools/bin/peribolos "$@"
