745c7cd69c4924eb13ee78978f668b77dfe694c0
二手掉包工程师 <rustin.liu@gmail.com>
2020-12-03 17:31:56 +0800

    feat: support org peribolos (#34)
    * config: add peribolos.sh
    
    * config: fix typo
    
    * config: fix name
    
    * config: add job
    
    * config: fix typo
    
    * config: fix typo
    
    * config: fix typo
    
    * config: fix typo
    
    * config: fix typo
    
    * config: fix typo
    
    * config: fix typo
    
    * config: refine config
    
    * config: fix typo
    
    * config: add env
    
    * config: fix typo
    
    * config: fix typo
    
    * config: add echo
    
    * config: fix job
    
    * config: delete job
    
    * config: refactor and test
    
    * config: test
    
    * config: revert
    
    * config: fix order
    
    * config: add new line
    
    * config: fix typo
    
    * config: refine label sync
    
    * config: refine config
    
    * config: fix string

---
+++ scripts/peribolos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Build merge and peribolos tools..."
+go build -o tools/bin/merge cmd/merge/main.go
+go build -o tools/bin/peribolos k8s.io/test-infra/prow/cmd/peribolos
+
+echo "Generate configuration file:"
+tools/bin/merge --org-part=tidb-community-bots=orgs/tidb-community-bots/org.yaml --merge-teams=true 2>&1 | tee orgs/config.yaml
+
+echo "Test the generated file"
+go test ./...
+
+echo "Start peribolos..."
+tools/bin/peribolos "$@"
