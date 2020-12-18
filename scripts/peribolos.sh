0ffb57b88690dd4d20c4329ecaa4522dc616d203
二手掉包工程师 <rustin.liu@gmail.com>
2020-12-18 15:23:17 +0800

    chore: rename org (#57)

--- scripts/peribolos.sh
+++ scripts/peribolos.sh
@@ -5,7 +5,7 @@ go build -o tools/bin/merge cmd/merge/main.go
 go build -o tools/bin/peribolos k8s.io/test-infra/prow/cmd/peribolos
 
 echo "Generate configuration file:"
-tools/bin/merge --org-part=tidb-community-bots=orgs/tidb-community-bots/org.yaml --merge-teams=true 2>&1 | tee orgs/config.yaml
+tools/bin/merge --org-part=ti-community-infra=orgs/ti-community-infra/org.yaml --merge-teams=true 2>&1 | tee orgs/config.yaml
 
 echo "Test the generated file"
 go test ./...
