2493953523437a83f7c8c45b66bde16ac42c3e65
Mini256 <minianter@foxmail.com>
2021-06-16 16:46:37 +0800

    config: invite mini-bot into org (#353)
    * config: invite mini-bot into org
    
    * fix: sort the logins
    
    * fix: add mod flag
    
    * fix: add comment

--- scripts/peribolos.sh
+++ scripts/peribolos.sh
@@ -2,7 +2,9 @@
 
 echo "Build merge and peribolos tools..."
 go build -o tools/bin/merge cmd/merge/main.go
-go build -o tools/bin/peribolos k8s.io/test-infra/prow/cmd/peribolos
+# Add -mod=mod flag for go build because the default behavior of go build have changed in go 1.16.
+# Refer: https://github.com/golang/go/issues/44212#issuecomment-776937327
+go build -mod=mod -o tools/bin/peribolos k8s.io/test-infra/prow/cmd/peribolos
 
 echo "Generate configuration file:"
 tools/bin/merge --org-part=ti-community-infra=orgs/ti-community-infra/org.yaml --merge-teams=true 2>&1 | tee orgs/config.yaml
