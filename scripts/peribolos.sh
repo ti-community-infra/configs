51f8670c76612dd76b1aa35986827f58d434e694
mini256 <minianter@foxmail.com>
2021-06-16 16:36:25 +0800

    fix: add comment

--- scripts/peribolos.sh
+++ scripts/peribolos.sh
@@ -2,6 +2,8 @@
 
 echo "Build merge and peribolos tools..."
 go build -o tools/bin/merge cmd/merge/main.go
+# Add -mod=mod flag for go build because the default behavior of go build have changed in go 1.16.
+# Refer: https://github.com/golang/go/issues/44212#issuecomment-776937327
 go build -mod=mod -o tools/bin/peribolos k8s.io/test-infra/prow/cmd/peribolos
 
 echo "Generate configuration file:"
