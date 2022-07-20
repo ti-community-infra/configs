71135410b1c811fe5c11df0eeca7b479a635c557
wuhuizuo <wuhuizuo@126.com>
2022-07-20 15:17:09 +0800

    config: add `tide/merge-method-*` pr labels (#600)

--- scripts/update-labels.sh
+++ scripts/update-labels.sh
@@ -2,17 +2,18 @@
 
 echo "Build label_sync..." >&2
 
-# Add -mod=mod flag for go build because the default behavior of go build have changed in go 1.16.
-# Refer: https://github.com/golang/go/issues/44212#issuecomment-776937327
-go build -mod=mod -o tools/bin/label_sync k8s.io/test-infra/label_sync
-
 label_sync=$PWD/tools/bin/label_sync
-
-cd prow/config || exit
+pushd $(mktemp -d)
+  git clone https://github.com/kubernetes/test-infra.git
+  cd test-infra/label_sync
+  go build -o ${label_sync}
+popd
 
 # Generate labels.
-"$label_sync" \
-  --config=labels.yaml \
-  --action=docs \
-  --docs-template=labels.md.tmpl \
-  --docs-output=labels.md
+pushd prow/config || exit
+  "$label_sync" \
+    --config=labels.yaml \
+    --action=docs \
+    --docs-template=labels.md.tmpl \
+    --docs-output=labels.md
+popd
