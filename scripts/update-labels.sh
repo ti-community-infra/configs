8794220be98b9a9ded028a6aa26c26ac8e8b5032
wuhuizuo <wuhuizuo@126.com>
2022-10-12 18:48:44 +0800

    chore(scripts): optimize update-labels.sh to avoid clone repo multi times
    optimize speed for local development

--- scripts/update-labels.sh
+++ scripts/update-labels.sh
@@ -3,11 +3,15 @@
 echo "Build label_sync..." >&2
 
 label_sync=$PWD/tools/bin/label_sync
-pushd $(mktemp -d)
-  git clone https://github.com/kubernetes/test-infra.git
-  cd test-infra/label_sync
-  go build -o ${label_sync}
-popd
+if [ -f $label_sync ]; then
+  echo 'tool existed.'
+else
+  pushd $(mktemp -d)
+    git clone https://github.com/kubernetes/test-infra.git
+    cd test-infra/label_sync
+    go build -o ${label_sync}
+  popd
+fi
 
 # Generate labels.
 pushd prow/config || exit
