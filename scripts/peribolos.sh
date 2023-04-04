9cdb09587bfc2951a7c0f7b06686e468b57def02
wuhuizuo <wuhuizuo@126.com>
2023-04-04 15:50:57 +0800

    chore(orgs/ti-community-infra): delete teams sync configs for archived repos (#832)

--- scripts/peribolos.sh
+++ scripts/peribolos.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-echo "Build merge and peribolos tools..."
-go build -o tools/bin/merge cmd/merge/main.go
-# Add -mod=mod flag for go build because the default behavior of go build have changed in go 1.16.
-# Refer: https://github.com/golang/go/issues/44212#issuecomment-776937327
-go build -mod=mod -o tools/bin/peribolos k8s.io/test-infra/prow/cmd/peribolos
-
 echo "Generate configuration file:"
-tools/bin/merge --org-part=ti-community-infra=orgs/ti-community-infra/org.yaml --merge-teams=true 2>&1 | tee orgs/config.yaml
+go run cmd/merge/main.go --org-part=ti-community-infra=orgs/ti-community-infra/org.yaml --merge-teams=true 2>&1 | tee orgs/config.yaml
 
 echo "Test the generated file"
 go test ./...
 
+peribolos_bin=$PWD/tools/bin/peribolos
+if [ -f $peribolos_bin ]; then
+    echo 'tool existed.'
+else
+    pushd $(mktemp -d)
+    git clone --depth 1 https://github.com/kubernetes/test-infra.git
+    cd test-infra/prow/cmd/peribolos
+    go build -o ${peribolos_bin}
+    popd
+fi
 echo "Start peribolos..."
 tools/bin/peribolos "$@"
